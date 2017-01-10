24216a7765e76bea3103f1b11e32d1a4dc40db33
Philip Gutierrez <pgutierrez@owens.com>
2017-01-09 21:08:08 -0500

    Added sidebar and article components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+class Article {
+  constructor(
+    public title: string,
+    public description: string) { }
+}
+
+@Component({
+  selector: 'app-sidebar',
+  template: `
+    <div id="sidebar">
+      sidebar will go here
+    </div>
+  `
+})
+export class SidebarComponent { }
+
+@Component({
+  selector: 'app-article',
+  template: `
+    <div>
+      <h2>{{article.title}}</h2>
+      <p>{{article.description}}</p>
+    </div>
+  `
+})
+export class ArticleComponent {
+  @Input()
+  article: Article;
+}
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <div id="container">
+      <app-sidebar></app-sidebar>
+      <div id="content">
+        <app-article 
+          *ngFor="let article of articles"
+          [article]="article" >
+        </app-article>
+      </div>
+    </div>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  articles: Article[];
+
+  constructor() {
+    this.articles = [
+      new Article('The Angular 2 screencast', 'The easiest way to learn Angular 2 is with Fullstack.io!'),
+      new Article('Fullstack React', 'Want to learn React too?'),
+      new Article('Vue is new', 'And pretty cool syntax too'),
+      new Article('But what about elm?', 'Everybody likes elm...right?')
+    ];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { AppComponent, SidebarComponent, ArticleComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule,
