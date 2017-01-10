ecb1c247ee3fe5f4fb9629ee08982a076f9ffcae
Philip Gutierrez <pgutierrez@owens.com>
2017-01-10 01:55:09 -0500

    Integrated with newsapi.org

--- src/app/article-list/article-list.component.ts
+++ src/app/article-list/article-list.component.ts
@@ -13,11 +13,11 @@ export class ArticleListComponent implements OnInit {
   @Input()
   articles: Article[];
 
-  constructor(private articleService: ArticleService) {
+  constructor(private articleService: ArticleService) { }
+
+  ngOnInit() {
     this.articleService.getArticles()
       .then(articles => this.articles = articles);
   }
-
-  ngOnInit() { }
 
 }
--- src/app/article.service.ts
+++ src/app/article.service.ts
@@ -1,22 +1,39 @@
 import { Injectable } from '@angular/core';
+import { Http, URLSearchParams } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 
 import { Article } from './article';
+
+const baseUrl = 'https://newsapi.org';
+const newsApiKey = '15c8df2fc03f472a87eb0d164dda26f2';
 
 @Injectable()
 export class ArticleService {
 
-  constructor() { }
+  constructor(private http: Http) { }
 
   public getArticles(): Promise<Article[]> {
-    return new Promise(resolve => {
-      setTimeout(() => {
-        resolve([
-          new Article('The Angular 2 screencast', 'The easiest way to learn Angular 2 is with Fullstack.io!'),
-          new Article('Fullstack React', 'Want to learn React too?'),
-          new Article('Vue is new', 'And pretty cool syntax too'),
-          new Article('But what about elm?', 'Everybody likes elm...right?')
-        ]);
-      }, 1000);
-    });
+    let params = new URLSearchParams();
+    params.set('apiKey', newsApiKey);
+    params.set('source', 'reddit-r-all');
+    return this.http
+      .get(`${baseUrl}/v1/articles`, {
+        search: params
+      })
+      .toPromise()
+      .then(resp => resp.json())
+      .then(json => json.articles)
+      .then(articles => {
+        const list = articles
+          .map(article => new Article(
+            article.title,
+            article.description,
+            article.urlToImage
+          ));
+        return list;
+      })
+      .catch(err => {
+        console.log('We got an error ' + err);
+      });
   }
 }
